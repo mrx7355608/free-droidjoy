d11650d9290eddf05e10729b9801490b8ee260dc
mrx7355608 <mrx7355608@gmail.com>
2025-05-26 08:00:21 +0500

    added back commented stuff

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,29 @@
 import socketio
 import eventlet
 from utils import notify_tkinter
+from xbox_simulator import XboxSimulator
 
 sio = socketio.Server(logger=True, cors_allowed_origins=[
     'http://localhost:8081',
 ])
 app = socketio.WSGIApp(sio)
+xbox_simulator = XboxSimulator()
+
+@sio.event
+def button_press(sid, data):
+    button = data['button']
+    action = data['action']
+    xbox_simulator.button_press(button, action)
+
+
+@sio.event
+def joystick(sid, data):
+    x = data["x"]
+    y = data["y"]
+    side = data["side"]
+    xbox_simulator.simulate_joystick(x, y, side)
+
+
 
 @sio.event
 def connect(sid, environ):
@@ -24,100 +42,10 @@ def disconnect(sid, reason):
         print('disconnect reason:', reason)
 
 
+
+
+
 def start_server():
     eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
 
-''' UNCOMMENT THIS ON WINDOWS TO SIMULATE A GAMEPAD
-gamepad = vg.VX360Gamepad()
-
-
-buttons_dict = {
-    "A": vg.XUSB_BUTTON.XUSB_GAMEPAD_A,
-    "B": vg.XUSB_BUTTON.XUSB_GAMEPAD_B,
-    "X": vg.XUSB_BUTTON.XUSB_GAMEPAD_X,
-    "Y": vg.XUSB_BUTTON.XUSB_GAMEPAD_Y,
-    "LB": vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER,
-    "RB": vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER,
-    "BACK": vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK,
-    "START": vg.XUSB_BUTTON.XUSB_GAMEPAD_START,
-    "LS": vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB,
-    "RS": vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_THUMB,
-    "DPAD-UP": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP,
-    "DPAD-DOWN": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN,
-    "DPAD-LEFT": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT,
-    "DPAD-RIGHT": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT
-}
-
-def press_button(button):
-    key = buttons_dict[button]
-    gamepad.press_button(button=key)
-    gamepad.update()
-    time.sleep(0.1)
-    gamepad.release_button(button=key)
-    gamepad.update()
-
-def scale_axis(x, y):
-    scaled_x = int(x * 32767)
-    scaled_y = int(-y * 32767)  # Note the negative sign to invert Y-axis
-    return scaled_x, scaled_y
-
-def simulate_joystick(x, y, side):
-    jx, jy = scale_axis(x, y)
-    if side == "left":
-        gamepad.left_joystick(x_value=jx, y_value=jy)
-        gamepad.update()
-    elif side == "right":
-        gamepad.right_joystick(x_value=jx, y_value=jy)
-        gamepad.update()
-
-
-@app.route('/joystick', methods=['POST'])
-def joystick():
-    data = request.get_json()
-
-    x = data["x"]
-    y = data["y"]
-    side = data["side"]
-
-    simulate_joystick(x, y, side)
-
-    return "OK"
-
-
-@app.route('/button', methods=['POST'])
-def button():    
-    data = request.get_json()
-
-    button = data.get("button")
-    press_button(button.upper())
-
-    return "OK"
-
-    
-
-@app.route('/trigger', methods=['POST'])
-def trigger():
-    data = request.get_json()
-    trigger_button = data["trigger"]
-    action = data["action"]
-
-    # Handle right trigger
-    if trigger_button == "RT":
-        if action == "press":
-            gamepad.right_trigger(255)
-        elif action == "release":
-            gamepad.right_trigger(0)
-        gamepad.update()
-
-    # Handle left trigger
-    elif trigger_button == "LT":
-        if action == "press":
-            gamepad.left_trigger(255)
-        elif action == "release":
-            gamepad.left_trigger(0)
-        gamepad.update()
-
-    return "OK"
-
-'''
 
---
+++ backend/xbox_simulator.py
@@ -0,0 +1,69 @@
+import vgamepad as vg
+
+class XboxSimulator:
+    def __init__(self):
+        self.gamepad = vg.VX360Gamepad()
+        self.buttons_dict = {
+            "A": vg.XUSB_BUTTON.XUSB_GAMEPAD_A,
+            "B": vg.XUSB_BUTTON.XUSB_GAMEPAD_B,
+            "X": vg.XUSB_BUTTON.XUSB_GAMEPAD_X,
+            "Y": vg.XUSB_BUTTON.XUSB_GAMEPAD_Y,
+            "LB": vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER,
+            "RB": vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER,
+            "BACK": vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK,
+            "START": vg.XUSB_BUTTON.XUSB_GAMEPAD_START,
+            "LS": vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB,
+            "RS": vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_THUMB,
+            "DPAD-UP": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP,
+            "DPAD-DOWN": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN,
+            "DPAD-LEFT": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT,
+            "DPAD-RIGHT": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT
+        }
+
+    def simulate_joystick(self, x, y, side):
+        jx, jy = scale_axis(x, y)
+        if side == "left":
+            self.gamepad.left_joystick(x_value=jx, y_value=jy)
+        elif side == "right":
+            self.gamepad.right_joystick(x_value=jx, y_value=jy)
+        
+        self.gamepad.update()
+
+
+    def button_press(self, button, action):
+        if button == "RT" or button == "LT":
+            self.handle_trigger(button, action)
+        else:
+            key = buttons_dict['button']
+            if action == 'press':
+                self.gamepad.press_button(button=key)
+            elif action == 'release':
+                self.gamepad.release_button(button=key)
+        
+            self.gamepad.update()
+        
+
+    def handle_trigger(self, trigger_button, action):
+        # Handle right trigger
+        if trigger_button == "RT":
+            if action == "press":
+                self.gamepad.right_trigger(255)
+            elif action == "release":
+                self.gamepad.right_trigger(0)
+
+        # Handle left trigger
+        elif trigger_button == "LT":
+            if action == "press":
+                self.gamepad.left_trigger(255)
+            elif action == "release":
+                self.gamepad.left_trigger(0)
+        
+        self.gamepad.update()
+
+
+    def scale_axis(self, x, y):
+        scaled_x = int(x * 32767)
+        scaled_y = int(-y * 32767)  # Note the negative sign to invert Y-axis
+        return scaled_x, scaled_y
+
+
